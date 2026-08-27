7b42a4f6b07a39aab7402afe8ce98720162baf5f
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-27 16:09:44 +0700

    test: cover portfolio report template ids

---
+++ test/features/portfolio_documents/domain/portfolio_report_template_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:portfolio/features/portfolio_documents/domain/reporting/portfolio_report_template.dart';
+
+void main() {
+  test('report template ids stay stable for report integrations', () {
+    expect(
+      PortfolioReportTemplateId.portfolioFull.value,
+      'portfolio_full',
+    );
+    expect(
+      PortfolioReportTemplateId.resumeCompact.value,
+      'resume_compact',
+    );
+  });
+}
