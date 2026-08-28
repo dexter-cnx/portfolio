8af574a519174dd98178d8a736cf7581cbc71b35
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-28 13:32:25 +0700

    feat(nav): register public portfolio routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
+import '../features/portfolio/presentation/pages/experience_page.dart';
 import '../features/portfolio/presentation/pages/home_page.dart';
+import '../features/portfolio/presentation/pages/open_source_page.dart';
+import '../features/portfolio/presentation/pages/projects_page.dart';
 import '../features/project_selection/presentation/pages/project_selection_admin_page.dart';
 import 'theme/app_scroll_behavior.dart';
 import 'theme/app_theme.dart';
@@ -19,7 +22,12 @@ class PortfolioApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      routes: {'/admin/projects': (_) => const ProjectSelectionAdminPage()},
+      routes: {
+        '/projects': (_) => const ProjectsPage(),
+        '/open-source': (_) => const OpenSourcePage(),
+        '/experience': (_) => const ExperiencePage(),
+        '/admin/projects': (_) => const ProjectSelectionAdminPage(),
+      },
       home: PortfolioHomePage(
         onLocaleChanged: (newLocaleCode) {
           context.setLocale(Locale(newLocaleCode));
